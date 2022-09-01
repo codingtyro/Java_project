69065285f9a4d9e1c10912b09ba2c724a80dd58a
codingtyro <2582678333@qq.com>
2022-09-02 00:11:16 +0800

    4

--- demo/error.java
+++ demo/error.java
@@ -2,6 +2,6 @@ package demo;
 
 public class error {
     public static void main(String[] args) {
-        System.out.println("today make a lot error");
+        System.out.println("today make a lot errors");
     }
 }
